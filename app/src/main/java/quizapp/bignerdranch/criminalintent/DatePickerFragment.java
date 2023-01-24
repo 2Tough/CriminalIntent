07dd2720c6f2fa6de165a3cb93458b9bf8efc9b8
Polaski <vainferro@gmail.com>
2023-01-24 18:44:54 +0300

    Date now showing properly.
    Set DatePicker in layout.

--- app/src/main/java/quizapp/bignerdranch/criminalintent/DatePickerFragment.java
+++ app/src/main/java/quizapp/bignerdranch/criminalintent/DatePickerFragment.java
@@ -3,6 +3,8 @@ package quizapp.bignerdranch.criminalintent;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,7 +14,11 @@ public class DatePickerFragment extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        View v = LayoutInflater.from(getActivity())
+                .inflate(R.layout.dialog_date, null);
+
         return new AlertDialog.Builder(getActivity())
+                .setView(v)
                 .setTitle(R.string.date_picker_title)
                 .setPositiveButton(android.R.string.ok, null)
                 .create();
