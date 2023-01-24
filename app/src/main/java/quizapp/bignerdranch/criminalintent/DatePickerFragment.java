c2cb736c48d00b8b9c9bfdfa8b6fd24cdcc642e0
Polaski <vainferro@gmail.com>
2023-01-24 13:50:53 +0300

    Created date button.

--- app/src/main/java/quizapp/bignerdranch/criminalintent/DatePickerFragment.java
+++ app/src/main/java/quizapp/bignerdranch/criminalintent/DatePickerFragment.java
@@ -1,4 +1,20 @@
 package quizapp.bignerdranch.criminalintent;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
 public class DatePickerFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.date_picker_title)
+                .setPositiveButton(android.R.string.ok, null)
+                .create();
+    }
 }
