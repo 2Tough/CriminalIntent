8da63dfa4ff580c54680b4ced72746fc1e96b586
Valentin <vainferro@gmail.com>
2023-05-10 23:07:54 +0300

    Added comments.

--- app/src/main/java/quizapp/bignerdranch/criminalintent/CrimeFragment.java
+++ app/src/main/java/quizapp/bignerdranch/criminalintent/CrimeFragment.java
@@ -124,5 +124,6 @@ public class CrimeFragment extends Fragment {
 
     private void updateDate() {
         mDateButton.setText(mCrime.getDate().toString());
+        // Date is not updating properly
     }
 }
